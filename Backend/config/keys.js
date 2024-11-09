5ace6222eced88437cb9124dab8dffdf71ef991b
ishanmathur28 <ishanmathur8791@gmail.com>
2024-11-09 21:50:34 +0530

    mongo connected

--- Backend/config/keys.js
+++ Backend/config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost:27017/lostFound',
-    secretOrKey: 'JaskaranSingh'
+    // mongoURI: 'mongodb://localhost:27017/lostFound',
+    mongoURI: 'mongodb+srv://imdsa:imdsa@cluster0.uankn.mongodb.net/LostAndFound?retryWrites=true&w=majority&appName=Cluster0',
+    secretOrKey: 'IshanMathur'
   };
   
